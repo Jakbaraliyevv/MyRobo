ac859c99bb03d1bb96ad712a709b1b91875092d5
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-05-07 15:24:30 +0500

    footer

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../footer";
 
 function Layout() {
   return (
@@ -9,6 +10,7 @@ function Layout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
